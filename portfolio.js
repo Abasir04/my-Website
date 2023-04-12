97ad523e1a5cfc4b06e629753a4ab9c3e4172e59
Adebayo Abdulbasir <103314318+Abasir04@users.noreply.github.com>
2023-04-12 13:11:38 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.multiple-typed', 
+{
+    strings: ['Front-End Developer', 'Blogger', 'Web Designer', 'Programmer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
